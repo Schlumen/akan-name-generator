77cdccca6362de3b45f466a27305c39c944f23f7
Schlumen <lumen.haendler@web.de>
2023-01-22 13:37:39 +0100

    Add functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+{
+    let names = {
+        female: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"],
+        male: ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    }
+
+    $(".name-button").on("click", () => {
+        let day = new Date($("#datepicker").val()).getUTCDay();
+        let gender = $("input[name='gender']:checked").val();
+
+        if (day && gender) {
+            $(".name-output").empty();
+            $(".name-output").append("<p>Your Akan Name is:</p>");
+            $(".name-output").append(`<h1>${names[gender][day]}</h1>`);
+        } else {
+            alert("Please enter date of birth and gender!");
+        }
+    });
+}
